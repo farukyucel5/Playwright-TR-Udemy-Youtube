f4aab6ab888d5cba26f7489c449174d2cb6f4fa3
farukyucel5 <farukyucel5@gmail.com>
2023-06-23 23:43:51 +0300

    The first test automation

--- tests/FirstTest.spec.ts
+++ tests/FirstTest.spec.ts
@@ -1,8 +1,10 @@
 import {test,expect} from '@playwright/test';
 
+
+
 test('First Test',async({page})=>{
    await page.goto("https://www.demoblaze.com/");
    await expect(page).toHaveTitle('STORE');
    await expect(page).toHaveURL('https://www.demoblaze.com/');
-
+   
 });
