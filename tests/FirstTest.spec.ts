9770e72a404604a3da57583b40631b639d2bcec9
farukyucel5 <farukyucel5@gmail.com>
2023-06-25 23:48:33 +0300

    jenkins integration update

--- tests/FirstTest.spec.ts
+++ tests/FirstTest.spec.ts
@@ -1,10 +1,20 @@
 import {test,expect} from '@playwright/test';
 
+test.beforeEach(async ({ page }) => {
+   await page.goto('https://www.demoblaze.com/');
+ });
+ 
 
+test.describe("Home page",()=>{
 
-test('First Test',async({page})=>{
-   await page.goto("https://www.demoblaze.com/");
-   await expect(page).toHaveTitle('STORE');
-   await expect(page).toHaveURL('https://www.demoblaze.com/');
+   test('Verify that the website is accessable',async({page})=>{
+      await expect(page).toHaveTitle('STORE');
+      await expect(page).toHaveURL('https://www.demoblaze.com/');
+      
+   });
    
-});
+
+
+})
+
+
