651ce4f85fb236d0b7f6a60499312831921904a8
farukyucel5 <farukyucel5@gmail.com>
2023-06-26 13:14:01 +0300

    Verify that all the links on the sidebar are up and running

--- tests/FirstTest.spec.ts
+++ tests/FirstTest.spec.ts
@@ -12,7 +12,7 @@ test.describe("Home page",()=>{
       
    });
 
-   test('Verify that all the buttons on the navbar are up and running',async({page})=>{
+   test('Verify that all the links on the navbar are up and running',async({page})=>{
      await expect(page.getByText("Home ")).toBeEnabled();
      await expect(page.getByText("Cart")).toBeEnabled();
      const cartLocate= page.getByText("Cart");
@@ -26,7 +26,27 @@ test.describe("Home page",()=>{
      contactLink.click();
      let contactVerificationString:string;
      contactVerificationString ="Send message";
+     await page.locator("xpath=(//button[@class='close'])[1]").click();
      await expect(page.getByText(contactVerificationString)).toBeVisible();
+     const loginlink= page.getByText("Log in").nth(2);
+     await expect(loginlink).toBeEnabled();
+     await loginlink.click();
+     const closeButtonLocate= page.locator("xpath=(//button[@class='close'])[3]");
+     await expect(closeButtonLocate).toBeVisible();
+     await closeButtonLocate.click();
+
+    });
+
+   test('Verify that all the links on the sidebar are up and running',async({page})=>{
+       await expect(page.getByText("Phones")).toBeVisible();
+       await expect(page.getByText("Phones")).toBeEnabled();
+
+       await expect(page.getByText("Laptops")).toBeVisible();
+       await expect(page.getByText("Laptops")).toBeEnabled();
+
+       await expect(page.getByText("Monitors")).toBeVisible();
+       await expect(page.getByText("Monitors")).toBeEnabled();
+
     });
    
 
