6ae27a3be9fb57c2c399cba3b44d174d0d2fddab
farukyucel5 <farukyucel5@gmail.com>
2023-06-29 23:37:37 +0300

    update

---
+++ tests/DemoBlazePack/OrangeDemo.spec.ts
@@ -0,0 +1,15 @@
+import{test,expect} from '@playwright/test';
+
+test.beforeEach(async({page})=>{
+
+    await page.goto("https://opensource-demo.orangehrmlive.com/web/index.php/auth/login")
+});
+
+test("Build in locators",async({page})=>{
+    await expect(page.getByAltText("company-branding")).toBeVisible();
+    await expect(page.getByText("Forgot your password? ")).toBeVisible();
+    await page.getByPlaceholder("Username").fill("Admin");
+    await page.getByPlaceholder("Password").fill("admin123");
+    await page.getByRole("button",{name:" Login "}).click();
+
+})
