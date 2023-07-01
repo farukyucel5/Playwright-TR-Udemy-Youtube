b54679bb0c3dafb107cd6ac1acdac61024aefefb
farukyucel5 <farukyucel5@gmail.com>
2023-07-01 19:44:20 +0300

    Udemy lessons associated with hard assertions

--- tests/YoutubePcak/Assertions.spec.ts
+++ tests/YoutubePcak/Assertions.spec.ts
@@ -19,6 +19,26 @@ test.describe("Assertions",async()=>{
         await expect(page.locator("//input[@id='name']")).toBeEnabled();
         // await expect(page.locator("//input[@id='name']")).toBeDisabled();
         await expect(page.locator("//input[@id='name']")).toBeEditable();
+        await expect(page.locator("//input[@id='name']")).toBeEmpty();
+        await page.locator("//input[@id='name']").fill("Faruk");
+        await expect(page.locator("//input[@id='name']")).toHaveValue("Faruk");
+        await expect(page.locator("//input[@id='name']")).not.toBeEmpty();
+        await expect(page.locator("//input[@id='name']")).toBeEmpty();
+
+
+     })
+
+     test("DemoQA",async({page})=>{
+      await page.goto("https://demoqa.com/")
+      await expect(page.getByText("Book Store Application")).not.toBeInViewport();
+      // await expect(page.getByText("Book Store Application")).toBeInViewport();
+      await expect(page.getByText("Book Store Application")).toContainText("Application");
+      await expect(page.getByText("Book Store Application")).toContainText(" Application");
+      await expect(page.getByText("Book Store Application")).toContainText("ion");
+      await expect(page.getByText("Book Store Application")).toHaveText("Book Store Application");
+      // await expect(page.getByText("Book Store Application")).toHaveText(" Book Store Application");
+      await expect(page.getByText("Book Store Application")).toHaveText("Store Application");
+      
 
      })
 
