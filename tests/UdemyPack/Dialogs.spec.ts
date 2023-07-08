0c43f1a16e5474b5329056d98716a5f7e6228c7a
farukyucel5 <farukyucel5@gmail.com>
2023-07-08 22:37:09 +0300

    confirm and prompt box

--- tests/UdemyPack/Dialogs.spec.ts
+++ tests/UdemyPack/Dialogs.spec.ts
@@ -19,7 +19,65 @@ test.describe("Dialogs",()=>{
 
     await page.click("//button[text()='Alert']");
 
-})
+  })
+
+
+  test("Confirm box",async({page})=>{
+    
+    page.on("dialog",dialog=>{
+       expect(dialog.type()).toBe("confirm");
+       expect(dialog.message()).toBe("Press a button!");
+      //dialog.accept();
+      dialog.dismiss();
+
+    })
+
+    await page.getByText("Confirm Box").click();
+    const resultText=page.locator("#demo");
+
+    expect(await resultText.textContent()).toBe("You pressed Cancel!");
+
+  })
+
+  test("Prompt",async({page})=>{
+    let promptValue:string;
+    promptValue="Faruk";
+    page.on("dialog",dialog=>{
+       expect(dialog.type()).toBe("prompt");
+       expect(dialog.message()).toBe("Please enter your name:");
+       expect(dialog.defaultValue()).toBe("Harry Potter");
+       dialog.accept(promptValue)
+     // dialog.dismiss();
+
+    })
+
+    await page.getByText("Prompt").click();
+    const resultText=page.locator("#demo");
+
+    expect(await resultText.textContent()).toBe(`Hello ${promptValue}! How are you today?`);
+
+  })
+
+
+  test("Prompt dismiss",async({page})=>{
+    let promptValue:string;
+    promptValue="Faruk";
+    page.on("dialog",dialog=>{
+       expect(dialog.type()).toBe("prompt");
+       expect(dialog.message()).toBe("Please enter your name:");
+       expect(dialog.defaultValue()).toBe("Harry Potter");
+       //dialog.accept(promptValue)
+       dialog.dismiss();
+
+    })
+
+    await page.getByText("Prompt").click();
+    const resultText=page.locator("#demo");
+
+    expect(await resultText.textContent()).toBe("User cancelled the prompt.");
+
+  })
+
 
 
 
