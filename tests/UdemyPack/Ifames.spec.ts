1f69c5676b0a8770d3d9f667d11bd6b63ba21157
farukyucel5 <farukyucel5@gmail.com>
2023-07-10 16:38:22 +0300

    Automation Practice Iframes

--- tests/UdemyPack/Ifames.spec.ts
+++ tests/UdemyPack/Ifames.spec.ts
@@ -26,8 +26,43 @@ test.describe("Frames",()=>{
 
     })
      
+})
 
 
-    
-  
+test.describe("Iframes Test Automation",()=>{
+
+  test.beforeEach(async({page})=>{
+
+    await page.goto("https://testautomationpractice.blogspot.com/");
   })
+
+  test("Iframe3",async({page})=>{
+     
+    const frame3=page.frame({url:"https://fs24.formsite.com/res/showFormEmbed?EParam=m_OmK8apOTDpwCqUlfXbL2rYe2Y6sJfY&796456169&EmbedId=796456169"});
+    const name:string="Faruk";
+    const textField=frame3?.locator("#RESULT_TextField-0");
+    await textField?.fill(name);
+    expect(await textField?.inputValue()).toBe(name);
+
+    const maleRadioBtn=frame3?.getByText("Male").nth(0);
+    await maleRadioBtn?.check();
+    expect(await maleRadioBtn?.isChecked()).toBeTruthy();
+
+    let date:string;
+    date="07/10/2024";
+
+    const dateField=frame3?.locator("id=RESULT_TextField-2");
+    await dateField?.fill(date);
+    expect(await dateField?.inputValue()).toBe(date);
+
+
+    const dropdown=frame3?.locator("id=RESULT_RadioButton-3");
+    const selectedInputValue="Radio-1";
+    await dropdown?.selectOption({value:selectedInputValue});
+    expect(await dropdown?.inputValue()).toBe(selectedInputValue);
+
+
+  })
+
+
+})
