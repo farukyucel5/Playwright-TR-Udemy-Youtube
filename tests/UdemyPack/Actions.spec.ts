e19b764299e4db6e6d32ae7a2017f496ccb05dd5
farukyucel5 <farukyucel5@gmail.com>
2023-07-13 00:03:29 +0300

    Right click actions

--- tests/UdemyPack/Actions.spec.ts
+++ tests/UdemyPack/Actions.spec.ts
@@ -5,12 +5,25 @@ test.describe("Actions",()=>{
  test("Hover actions",async({page})=>{
    await page.goto("https://www.amazon.com/");
    const helloSignInLink=page.locator("#nav-link-accountList");
+   await page.waitForSelector("#nav-link-accountList");
    await helloSignInLink.hover();
    await page.waitForTimeout(3000);
    const accountLink=page.locator("//span[text()='Account']");
    await accountLink.hover();
 
-})
+ })
+
+ test("Right click actions",async({page})=>{
+   await page.goto("https://demo.guru99.com/test/simple_context_menu.html");
+  const rightClickElement=page.getByText("right click me");
+  await rightClickElement.click();//normal mouse click
+
+  await rightClickElement.click({button:"right"});//sağ click
+  
+  const rightClickMenu=page.locator("//ul[@class='context-menu-list context-menu-root']");
+  await expect(rightClickMenu).toBeVisible();
+
+ })
 
 
 
