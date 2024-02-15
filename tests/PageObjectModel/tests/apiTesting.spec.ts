f7af055425aea14418cc572ab55d2dc90af4c6fd
farukyucel5 <farukyucel5@gmail.com>
2024-02-15 17:01:55 +0300

    get request

--- tests/PageObjectModel/tests/apiTesting.spec.ts
+++ tests/PageObjectModel/tests/apiTesting.spec.ts
@@ -2,11 +2,16 @@ import { test, expect } from '@playwright/test';
 
 
 test("get users",async({request})=>{
-
+    let response;
     await test.step("Sending GET request to the API", async () => {
-        const response = await request.get("https://reqres.in/api/users?page=2");
+        response = await request.get("https://postman-echo.com/get?foo1=bar1&foo2=bar2");
         const responseBody = await response.json();
         console.log(responseBody);
+        expect(response.status()).toBe(200);
+      });
+
+    await test.step("Verifying the response is 200", async () => {
+       
         expect(response.status()).toBe(200);
       });
 })
