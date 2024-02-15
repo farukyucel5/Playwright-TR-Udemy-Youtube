294c6d59aee52b82eba0470d076bc626bc522162
farukyucel5 <farukyucel5@gmail.com>
2024-02-15 22:48:13 +0300

    get request

--- tests/ApiTesting/tests/apiTesting.spec.ts
+++ tests/ApiTesting/tests/apiTesting.spec.ts
@@ -1,26 +1,17 @@
-import { test, expect } from '@playwright/test';
+import {test,expect} from '@playwright/test'
 
-
+let response;
+let responseBody;
 test("get users",async({request})=>{
-    let response;
-    let responseBody;
-    await test.step("Sending GET request to the API", async () => {
-        response = await request.get("https://reqres.in/api/users/2");
-        responseBody = await response.json();
-        console.log(responseBody);
-        expect(response.status()).toBe(200);
-      });
 
-    await test.step("Verifying the response is 200", async () => {
-       
-        expect(response.status()).toBe(200);
-        
-      });
-
-    await test.step("Verifying the id inside the responseBody", async () => {
-        responseBody = await response.json();
-        console.log(responseBody.data.id);
-        expect(responseBody.data.id).toBe(2);
-        
-      });  
+  response=await request.get("https://reqres.in/api/users/2");
+  responseBody=await response.json();
+  console.log(responseBody);
+  expect(response.status()).toBe(200);
+  expect(responseBody.data.id).toBe(2);
+  console.log(responseBody.data.id);
+  expect(responseBody.data.first_name).toBe("Janet")
+  console.log(responseBody.data.first_name);
+  expect(responseBody.data.email).toBe("janet.weaver@reqres.in");
+  console.log(responseBody.data.email);
 })
